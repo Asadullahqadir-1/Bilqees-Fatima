28642cfb9af4978fc50ae1344b43824ce62b6170
ASAD ULLAH QADIR <Asadullahqadir45@gmail.com>
2026-02-11 02:27:44 +0500

    chore: streamline footer copy

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,6 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-950 py-8 text-center text-gray-600 text-sm border-t border-white/5 font-sans">
       <p>© {new Date().getFullYear()} Bilqees Fatima. All rights reserved.</p>
-      <p className="mt-2">Designed with Professionalism & Precision.</p>
     </footer>
   );
 };
